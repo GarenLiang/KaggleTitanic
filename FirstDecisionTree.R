e530f15dd4a8aed731dbcf9c8cd238e70aff9250
JackLiang1 <ideabreeze@gmail.com>
2016-01-31 00:07:24 -0600

    Create FirstDecisionTree.R

---
+++ FirstDecisionTree.R
@@ -0,0 +1,21 @@
+# KaggleTitanic
+# Your train and test set are still loaded in
+
+str(train)
+str(test)
+
+# Build the decision tree
+my_tree_two <- rpart(Survived~Pclass+Sex+Age+SibSp+Parch+Fare+Embarked,data=train,method="class")
+
+# Visualize the decision tree using plot() and text()
+plot(my_tree_two)
+text(my_tree_two)
+
+# Load in the packages to create a fancified version of your tree
+library(rpart)
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
+# Time to plot your fancy tree
+fancyRpartPlot(my_tree_two)
